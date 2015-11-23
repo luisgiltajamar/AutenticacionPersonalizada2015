0674362071dc5dda19d7cfebf35e7d5edbb81e1d
luis@yo.es <luis@yo.es>
2015-11-23 14:27:24 +0100

    Con cifrado

---
+++ AutenticacionPersonalizada/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutenticacionPersonalizada.Utilidades;
+
+namespace AutenticacionPersonalizada.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var clave = ConfigurationManager.AppSettings["ClaveCifrado"];
+
+            var cifrado = SeguridadUtilidades.Cifrar(
+                            "Hola don pepito",clave );
+
+            var data = Convert.FromBase64String(cifrado);
+
+            var descifrado = SeguridadUtilidades.DesCifrar(
+                            data, clave);
+            return View();
+        }
+    }
+}
---
+++ AutenticacionPersonalizada/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutenticacionPersonalizada.MvcApplication" Language="C#" %>
