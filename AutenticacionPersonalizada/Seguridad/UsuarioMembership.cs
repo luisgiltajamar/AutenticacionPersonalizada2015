8ef34f87ba29a9db11de70e9ced1ed95c2ee0e76
luis@yo.es <luis@yo.es>
2015-11-24 10:06:00 +0100

    Con principal

---
+++ AutenticacionPersonalizada/Seguridad/UsuarioMembership.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+using AutenticacionPersonalizada.Models;
+using AutenticacionPersonalizada.Utilidades;
+
+namespace AutenticacionPersonalizada.Seguridad
+{
+  public class UsuarioMembership:MembershipUser
+    {
+        public int id { get; set; }
+        public string login { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string imagen { get; set; }
+        public String Rol { get; set; }
+
+      public UsuarioMembership(Usuario us)
+      {
+          var clave = ConfigurationManager.AppSettings["ClaveCifrado"];
+          id = us.id;
+          nombre = us.nombre;
+          apellidos = us.apellidos;
+          imagen = us.imagen;
+          Rol = us.Rol.nombre;
+          login = SeguridadUtilidades.DesCifrar(
+              Convert.FromBase64String(us.login), clave);
+      }
+
+    }
+}
